f45a986c41bdd8182818cfc0fa2c5a0404b7dba4
Monixc <chris13404@gmail.com>
2025-04-16 14:42:50 +0900

    create: 인프런 nestjs 강의 섹션 1~3 정리

---
+++ 황다경/code/inflearn-nest/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
+import { BoardsController } from './boards/boards.controller';
+@Module({
+  imports: [BoardsModule],
+  controllers: [BoardsController],
+})
+export class AppModule {}
