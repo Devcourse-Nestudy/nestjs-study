76d1be87843e61da03b2ce0b6906cb165074d5fe
Lee jeongchan <sjk338285@gmail.com>
2025-05-07 18:10:44 +0900

    email 실습

---
+++ 이정찬/code/ch4-email/src/email/email.service.ts
@@ -0,0 +1,50 @@
+import Mail = require('nodemailer/lib/mailer');
+import * as nodemailer from 'nodemailer';
+import * as dotenv from 'dotenv';
+
+import { Injectable } from '@nestjs/common';
+
+dotenv.config();
+
+interface EmailOptions {
+  to: string;
+  subject: string;
+  html: string;
+}
+
+@Injectable()
+export class EmailService {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+  }
+
+  async sendMemberJoinVerification(
+    emailAddress: string,
+    signupVerifyToken: string,
+  ) {
+    const baseUrl = 'http://localhost:3000'; // TODO: config
+
+    const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
+
+    const mailOptions: EmailOptions = {
+      to: emailAddress,
+      subject: '가입 인증 메일',
+      html: `
+        가입확인 버튼를 누르시면 가입 인증이 완료됩니다.<br/>
+        <form action="${url}" method="POST">
+          <button>가입확인</button>
+        </form>
+      `,
+    };
+
+    return await this.transporter.sendMail(mailOptions);
+  }
+}
